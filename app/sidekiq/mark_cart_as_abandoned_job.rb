16f9c8797cce89a19c8d3ad5205688f408a5b32f
Camily <camily.rocha07@gmail.com>
2025-11-17 01:22:10 -0300

    feat: implements abandoned cart cleaning job and scheduling.

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -2,6 +2,11 @@ class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
   def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
-  end
+# MARCAR como abandonado (sem interação há mais de 3 horas)
+    Cart.where('updated_at < ?', 3.hours.ago)
+        .where(abandoned_at: nil)
+        .update_all(abandoned_at: Time.current)
+
+    # REMOVER carrinhos abandonados há mais de 7 dias
+    Cart.where('abandoned_at < ?', 7.days.ago).destroy_all  end
 end
